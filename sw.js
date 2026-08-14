8e4f809f111fcb6d3d14504c28e2722db69d0ec8
Cap259 <ahmed01103970370@gmail.com>
2026-08-14 17:09:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='manager-lines-v25-profit-20260813-001';
+const CACHE='manager-lines-v26-all-updates-20260814-002';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim();})());});
